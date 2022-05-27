dd62f07278efb648c644ca82940797969b6708f6
alvarolobooooooooooooooooooo <105078434+alvarolobooooooooooooooooooo@users.noreply.github.com>
2022-05-27 14:07:39 -0600

    Paneles creados

---
+++ src/Cuenta.java
@@ -0,0 +1,32 @@
+
+public class Cuenta {
+    private String nombre;
+    private String pass;
+
+    public Cuenta(String nombre, String pass) {
+        this.nombre = nombre;
+        this.pass = pass;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    @Override
+    public String toString() {
+        return "Cuenta{" + "nombre=" + nombre + ", pass=" + pass + '}';
+    }
+    
+}
